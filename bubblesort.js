00f3ab9b2e1b1e4563738eae4a0eb33d18a05b1d
Nick Chen <chenyahua2012@hotmail.com>
2020-05-21 18:24:41 -0400

    bubble sort function done

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,20 @@
 function bubbleSort(array) {
-
-    /* your code here */
-    return [];
+    for(let j=0;j<array.length;j++){
+    for(let i=0;i<array.length;i++){
+    if(!isInOrder(array,i))swap(array,i);
+      console.log('compare',array[i],'with',array[i+1],'array be like' ,array)
+    }
+    }
+    return array;
+  }
+  
+  function isInOrder(array,index){
+      if(index===array.length-1)return true;
+      return array[index]<array[index+1];
+  }
+  
+  function swap (array,index){
+    let old = array[index];
+    array[index]=array[index+1];
+    array[index+1]=old;
   }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,10 @@ describe('Bubble Sort', function(){
     it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
     });
-  });
+    it('sorts an array of one element', function(){
+        expect( bubbleSort([7])).toEqual( [7] );
+    });
+    it('sorts an array of many elements', function(){
+        expect( bubbleSort([5,2,3,1,4,6]) ).toEqual([1,2,3,4,5,6]);
+    })
+});
